78d5875bc9ff86d8a27b9b76dc1f434ea9b85dcf
lissa <e-namestnikova@inbox.ru>
2022-07-05 00:16:15 +0700

    add tnird task

---
+++ 3_task/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+Console.Clear();
+int num = InputInt("введите число ");
+int a = 1;
+while (a <= num)
+{
+    a++; 
+    Console.WriteLine($"{a} * {a} * {a} = {a * a * a}");
+}
+Console.WriteLine();
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
